6a5dd3bb35c21eb767ebd1021e006faf11aada13
Justin <justinestradacruz2006@gmail.com>
2026-05-11 14:13:04 -0600

    Created service to fetch news and type server response

---
+++ src/service/NewsService.ts
@@ -0,0 +1,15 @@
+import type { ServerResponse } from "../types/ServerResponse";
+
+const API_KEY = '1d3f8e372d1748d6a56ebd76fb6588bc';
+const API_URL = 'https://newsapi.org/v2/';
+
+const getCostaRicaNews = async ():Promise<ServerResponse> => {
+    const response = await fetch(`${API_URL}top-headlines?country=cr&apiKey=${API_KEY}`);
+
+    if(!response.ok){
+        throw new Error('Cannot fetch new from Costa Rica');
+    }
+
+    const data:ServerResponse = await response.json();
+    return data;
+};
