87bd55135b42b0311c5b216339b3cce93648ef43
Justin <justinestradacruz2006@gmail.com>
2026-05-11 13:56:15 -0600

    Needed types created

---
+++ src/types/New.ts
@@ -0,0 +1,12 @@
+import type { Source } from "./Source";
+
+export interface New {
+  source: Source;
+  author: string;
+  title: string;
+  description: string;
+  url: string;
+  urlToImage: string;
+  publishedAt: string;
+  content: string;
+}
